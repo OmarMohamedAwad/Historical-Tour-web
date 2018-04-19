6f84465f41a8e6ad71ea1598c96a56470080cd1b
Omar Awad <omar.awad825@gmail.com>
2018-04-19 17:40:59 +0200

    Add the folder css and js and image and icon

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+//
+//function search(){
+//    var search =document.getElementById("search-input");
+//    
+//    search.style.width="60%";
+//}
+
+
+$(document).ready(function () {
+
+    "use strict";
+
+    $("html").niceScroll();
+});
